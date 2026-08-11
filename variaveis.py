f3b66c87cc3824b1d298918d0899f3bffd0473fa
Warley Lourenço de Sousa <warley.sousa8@gmail.com>
2026-08-11 19:07:38 -0300

    praticando

---
+++ variaveis.py
@@ -0,0 +1,14 @@
+"""times = {
+    1: "Corinthians",
+    2: "Cruzeiro",
+    3: "Palmeiras",
+    4: "São Paulo",
+    5: "Santos",
+    6: "Vasco",
+    7: "Fluminense",
+    8: "Botafogo",
+    9: "Atlético Mineiro",
+    10: "Flamengo",
+}
+for futebol in range(1, 11):
+    print(f"O maior time de todos começa com {times[futebol]}, seguido dos demais!")"""

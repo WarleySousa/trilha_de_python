00e80f337864b4ed11024b98f1142a5ccae9db0a
Warley Lourenço de Sousa <warley.sousa8@gmail.com>
2026-08-11 19:08:34 -0300

    praticando

--- variaveis.py
+++ variaveis.py
@@ -12,3 +12,18 @@
 }
 for futebol in range(1, 11):
     print(f"O maior time de todos começa com {times[futebol]}, seguido dos demais!")"""
+
+times = {
+    1: "Corinthians",
+    2: "Cruzeiro",
+    3: "Palmeiras",
+    4: "São Paulo",
+    5: "Santos",
+    6: "Vasco",
+    7: "Fluminense",
+    8: "Botafogo",
+    9: "Atlético Mineiro",
+    10: "Flamengo",
+}
+for futebol in range(1, 11):
+    print(f"O maior time de todos começa com {times[futebol]}, seguido dos demais!")
